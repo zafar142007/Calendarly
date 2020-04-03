8085c4b2352fe29bc0ceac03f2c4c2b4888930a5
zaa0000 <zafar.ansari@walmartlabs.com>
2020-04-03 15:12:24 +0530

    fixed a bug

--- src/main/java/com/zafar/calendarly/domain/response/CalendarResponse.java
+++ src/main/java/com/zafar/calendarly/domain/response/CalendarResponse.java
@@ -1,17 +1,16 @@
 package com.zafar.calendarly.domain.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zafar.calendarly.util.CalendarConstants;
 import java.time.Instant;
 
 
 /**
  * Generic response class for all APIs
+ *
  * @author Zafar Ansari
  */
 public class CalendarResponse {
 
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
   private Instant timestamp = Instant.now();
 
   private Double version = CalendarConstants.VERSION;
