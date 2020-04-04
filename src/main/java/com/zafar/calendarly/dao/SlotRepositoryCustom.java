80e170d950024b9ff457dc94e5ccf984ea74741d
Zafar <zafar142007@gmail.com>
2020-04-05 00:46:34 +0530

    preparing for heroku

--- src/main/java/com/zafar/calendarly/dao/SlotRepositoryCustom.java
+++ src/main/java/com/zafar/calendarly/dao/SlotRepositoryCustom.java
@@ -5,6 +5,9 @@ import java.time.Instant;
 import java.util.Set;
 
 /**
+ * In order to book a slot, it is required to lock the row we intend to update to allow concurrent
+ * bookings. So we need a custom implementation where we could put this in a transaction.
+ *
  * @author Zafar Ansari
  */
 public interface SlotRepositoryCustom {
